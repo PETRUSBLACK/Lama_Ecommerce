4c911a2f604b3f8dec57a718d18f1a39b4b8a01a
PETRUSBLACK <onukwupetrusoge@gmail.com>
2024-05-22 12:43:08 +0100

    starting encrypting with Cryptojs

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const User = require("../models/User")
-constr CryptoJS = require("crypto-js")
+const CryptoJS = require("crypto-js")
 
 //REGISTER
 router.post("/register", async (req,res)=>{
