3ce08871f6e6df5dd7c3267ce54e7e2d8493a71b
PETRUSBLACK <onukwupetrusoge@gmail.com>
2024-05-22 12:42:52 +0100

    starting encrypting with Cryptojs

--- models/Product.js
+++ models/Product.js
@@ -2,20 +2,16 @@ const mongoose = require("mongoose");
 
 const ProductSchema = new mongoose.Schema(
     {
-        userId:{type:String, required:true, unique:true},
-        products:[
-            {
-                productId:{
-                    type:String
-                },
-                quantity:{
-                    type:Number,
-                    default:1,
-                }
-            }
-        ],
+        title:{type:String, required:true, unique:true},
+        desc:{type:String, required:true,},
+        img:{type:Array},
+        categories:{type:String},
+        size:{type:String},
+        color:{type:String},
+        price:{type:Number, required:true},
+
     },
     {timestamps:true}
 )
 
-module.exports = mongoose.model("Product", UserSchema);
+module.exports = mongoose.model("Product", ProductSchema);
--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const User = require("../models/User")
+constr CryptoJS = require("crypto-js")
 
 //REGISTER
 router.post("/register", async (req,res)=>{
