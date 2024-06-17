95e4ff18062daf754ab86284ba3df49b2fdfa3f9
PETRUSBLACK <onukwupetrusoge@gmail.com>
2024-06-17 03:20:02 +0100

    get all user with sort and limit function

--- routes/user.js
+++ routes/user.js
@@ -42,6 +42,20 @@ router.get("/find/:id", verifyTokenAndAdmin, async (req,res)=>{
     }
 })
 
+// GET ALL USER
+router.get("/", verifyTokenAndAdmin, async (req,res)=>{
+
+    const query = req.query.new
+    try{
+        const users = query 
+        ? await User.find().sort({_id: -1}).limit(1) : await User.find();
+        res.status(200).json(users);
+    }catch(err){
+        res.status(500).json(err)
+    }
+})
+
+
 
 
 
