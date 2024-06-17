db3f91db6a72a72079f4b922c0bafa6c7e182802
PETRUSBLACK <onukwupetrusoge@gmail.com>
2024-06-17 02:36:41 +0100

    get a user

--- routes/user.js
+++ routes/user.js
@@ -1,4 +1,4 @@
-const {verifyTokenAndAuthorization, verifyToken} = require("./verifyToken");
+const {verifyTokenAndAuthorization, verifyTokenAndAdmin} = require("./verifyToken");
 const CryptoJS = require("crypto-js")
 const User = require("../models/User")
 
@@ -25,11 +25,23 @@ router.put("/:id", verifyTokenAndAuthorization ,async (req, res) => {
 router.delete("/:id", verifyTokenAndAuthorization, async (req,res)=>{
     try{
         await User.findByIdAndDelete(req.params.id)
-        res.status(200).json("user has been deleted ...")
+        res.status(200).json("user has been deleted ....")
     }catch(err){
         res.status(500).json(err)
     }
 })
+
+// GET USER
+router.get("/find/:id", verifyTokenAndAdmin, async (req,res)=>{
+    try{
+        const user = await User.findById(req.params.id);
+        const { password, ...others } = user._doc;
+        res.status(200).json({others});
+    }catch(err){
+        res.status(500).json(err)
+    }
+})
+
 
 
 
