69d38b73c7304209c31983068fb0540617e74e8e
PETRUSBLACK <onukwupetrusoge@gmail.com>
2024-06-17 04:04:05 +0100

    aggregate

--- routes/user.js
+++ routes/user.js
@@ -55,6 +55,25 @@ router.get("/", verifyTokenAndAdmin, async (req,res)=>{
     }
 })
 
+// GET USER STATS
+
+router.get("/stats", verifyTokenAndAdmin, async (req, res) => {
+    const date = new Date();
+    const lastyear = new Date(date.setFullYear(date.getFullYear() - 1));
+    
+    try{At
+
+        const data = await User.aggregate([
+            {$match: { createdAt: { $gte: lastyear }}},
+            {$project: { month: { $month: "$createdAt"}, }, },
+            {$group: { _id: "$month", total:{ $sum: 1 }, }, }
+        ])
+
+        res.status(200).json(data)
+    }catch(err){
+        res.status(500).json(err)}
+})
+
 
 
 
