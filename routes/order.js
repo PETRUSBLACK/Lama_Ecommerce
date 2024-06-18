c05023218f6092f7881758af583cabc4d0875514
PETRUSBLACK <onukwupetrusoge@gmail.com>
2024-06-18 02:32:19 +0100

    order put and post

--- routes/order.js
+++ routes/order.js
@@ -1,4 +1,72 @@
+const Order = require("../models/Order")
+const {
+    verifyTokenAndAuthorization, 
+    verifyTokenAndAdmin, 
+    verifyToken
+} = require("./verifyToken");
+
+
 const router = require("express").Router();
+
+// CREATE
+
+router.post("/", verifyToken, async (req, res) => {
+    const newOrder = new Order(req.body)
+
+    try{
+        const savedOrder = await newOrder.save();
+        res.status(200).json(savedOrder)
+    }catch(err){
+        res.status(500).json(err)
+    }
+})
+
+// UPDATE
+router.put("/:id", verifyTokenAndAdmin, async (req,res)=>{
+    try{
+        const updatedOrder = await Order.findByIdAndUpdate(
+            req.params.id,
+            {
+                $set: req.body,
+            },
+            {new : true}
+        );
+        res.status(200).json(updatedOrder);
+    }catch(err) {
+        res.status(500).json(err);
+    }
+})
+
+// DELETE
+router.delete("/:id", verifyTokenAndAuthorization, async (req,res) => {
+    try{
+        await Cart.findByIdAndDelete(req.params.id);
+        res.status(200).json("Product has been deleted ..."); 
+    }catch(err){
+        res.status(500).json(err);
+    }
+})
+
+// // GET USER CART
+router.get("/find/:userId", verifyTokenAndAuthorization, async (req,res)=>{
+    try{
+        const cart = await Cart.findOne({userId: req.params.userId});
+        res.status(200).json(cart);
+    }catch(err){
+        res.status(500).json(err)
+    }
+})
+
+// // GET ALL PRODUCT
+router.get("/", verifyTokenAndAdmin, async (req,res)=>{
+
+    try{
+        const carts = await Cart.find()
+        res.status(200).json(carts)
+    }catch(err){
+        res.status(500).json(err)
+    }
+})
 
 
 module.exports = router;
