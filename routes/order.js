c8dcfccccd3712222907bae2deaa062e40e246dc
PETRUSBLACK <onukwupetrusoge@gmail.com>
2024-06-19 20:58:18 +0100

    adding aggregating the monthly orders

--- routes/order.js
+++ routes/order.js
@@ -68,5 +68,19 @@ router.get("/", verifyTokenAndAdmin, async (req,res)=>{
     }
 })
 
+// GET MONTHLY INCOME
+router.get("/income", verifyTokenAndAdmin, async (req, res) => {
+
+    const date = new Date();
+    const lastMonth = new Date(date.setMonth(date.getMonth() - 1));
+    const previousMonth = new Date(new Date().setMonth(lastMonth.getMonth() - 1));
+
+    try{
+
+    }catch(err){
+        res.status(500).json(err)
+    }
+})
+
 
 module.exports = router;
